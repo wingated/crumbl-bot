9e04ea24d93a6bbca2c82bb2d726d97b2f72a4dd
David Wingate <wingated@cs.byu.edu>
2023-04-27 11:33:10 -0600

    tweaks

--- api/date.py
+++ api/date.py
@@ -1,3 +1,5 @@
+import os
+
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 
@@ -7,5 +9,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+        response = f"SID: {os.environ['TWILIO_ACCOUNT_SID']}, AUTH: {os.environ['TWILIO_AUTH_TOKEN']}, PHONE: {os.environ['TWILIO_FROM_NUMBER']}"
+        response += str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        self.wfile.write( response.encode() )
         return
